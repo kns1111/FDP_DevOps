b2d5017f101b88e735b514db8ae20b36a41e6e00
kns1111 <kalyaninikhilsalvi@gmail.com>
2026-01-01 23:51:56 +0530

    Update AdditionWithInput.java

--- AdditionWithInput.java
+++ AdditionWithInput.java
@@ -1,18 +1,7 @@
-import java.util.Scanner;
-
-public class AdditionWithInput {
+class AdditionWithInput {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter the first number: ");
-        double num1 = scanner.nextDouble();
-
-        System.out.print("Enter the second number: ");
-        double num2 = scanner.nextDouble();
-
-        double sum = num1 + num2;
-        System.out.println("The sum is: " + sum);
-
-        scanner.close(); // Close the scanner to release resources
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        System.out.println("Sum = " + (a + b));
     }
 }
