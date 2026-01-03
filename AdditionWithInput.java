939510725350abe654e94040cc281faa6666f772
kns1111 <kalyaninikhilsalvi@gmail.com>
2026-01-03 14:50:20 +0530

    Update AdditionWithInput.java

--- AdditionWithInput.java
+++ AdditionWithInput.java
@@ -1,7 +1,7 @@
-class AdditionWithInput {
-    public static void main(String[] args) {
+class AdditionWithInput 
+    public static void main(Str
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         System.out.println("Sum = " + (a + b));
-    }
-}
+    
+
